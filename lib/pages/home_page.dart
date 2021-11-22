631f230f6d45175f0172df1d3a97303008051ee9
bzamanbd <riponzamanbd@gmail.com>
2021-11-22 12:37:35 +0600

    done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import '../widgets/custom_btn.dart';
-class HomePage extends StatefulWidget {
+import '../widgets/rect_container.dart';
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int count=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,30 +14,22 @@ class _HomePageState extends State<HomePage> {
       // bottomNavigationBar: ,
       // bottomSheet: ,
       // floatingActionButton: ,
-      body:  Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:   <Widget>[
-            Text('Current Value is = $count',
-            style: const TextStyle(
-              fontSize: 18.0,
+          children: const <Widget>[
+            RectContainer(Colors.green),
+            SizedBox(
+              height: 20,
             ),
+            RectContainer(Colors.red),
+            SizedBox(
+              height: 20,
             ),
-            const SizedBox(
-              height: 22.0,
-            ),
-            InkWell(
-              onTap: (){
-                setState(() {
-                  count++;
-                });
-              },
-              child: const CustomBTN('Counter'),
-            ),
+            RectContainer(Colors.blue),
           ],
         ),
       ),
-      
     );
   }
 }
---
+++ lib/widgets/rect_container.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+class RectContainer extends StatelessWidget {
+  final bgColor;
+  const RectContainer(this.bgColor, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 150,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: bgColor,
+      ),
+    );
+  }
+}
