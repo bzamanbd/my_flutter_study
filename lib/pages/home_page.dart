366356cced3cf5c78222c23488097b9f6adcedd1
bzamanbd <riponzamanbd@gmail.com>
2021-11-24 22:58:10 +0600

    done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,20 +21,12 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.all(20.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                const CircleAvatar(
+              children:  const [
+                CircleAvatar(
                   backgroundImage: NetworkImage('https://oceanbeachsandiego.com/sites/default/files/d7/photos/newport-avenue-ocean-beach-flower-02.jpg'),
                   radius: 150,
                 ),
-                const SizedBox(height: 10,),
-                Container(
-                  width: 300,
-                  height: 300,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(image: NetworkImage('https://cdn.shopify.com/s/files/1/0549/8421/2633/files/750x1100_copy_720x.jpg?v=1615795396')),
-                  ),
-                ),
-                
+                SizedBox(height: 10,),
               ],
             ),
           ),
