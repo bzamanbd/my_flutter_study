c23b72e1238e23ec39c5545a695a2e8de9c6f089
bzamanbd <riponzamanbd@gmail.com>
2021-12-16 08:25:24 +0600

    done final

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,21 +28,26 @@ class _HomePageState extends State<HomePage> {
           children: [
             Container(
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.purple,width: 2.0)
+                border: Border.all(color: Colors.purple,width: 2.0),
               ),
               child: Checkbox(value: _cbValue1, onChanged: (status){setState(() {
                 _cbValue1 =status!;
-              });}),
+              });},
+              shape: const CircleBorder(),
+              ),
             ),
             Checkbox(value: _cbValue2, onChanged: (status){setState(() {
               _cbValue2=status!;
-            });}),
+            });},
+            ),
             Checkbox(value: _cbValue3, onChanged: (status){setState(() {
               _cbValue3=status!;
             });}),
             Checkbox(value: _cbValue1, onChanged: (status){setState(() {
               _cbValue1=status!;
-            });}),
+            });},
+            shape: const CircleBorder(),
+            ),
             
           ],
         ),
