a98c9b92ad0236230c94cb7eb4b808ec3e19fb77
bzamanbd <riponzamanbd@gmail.com>
2021-12-17 08:41:52 +0600

    done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,10 +8,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _groupValue=0;
-  void rmethod(value){
+  double _sValue=1.0;
+  void smethod(value){
     setState(() {
-      _groupValue=value;
+      _sValue = value;
     });
   }
   @override
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title,
         style: const TextStyle(
-          fontSize: 24.0
+          fontSize: 26.0,
         ),
         ),
         centerTitle: true,
@@ -29,60 +29,33 @@ class _HomePageState extends State<HomePage> {
       // bottomNavigationBar: ,
       // bottomSheet: ,
       // floatingActionButton: ,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RadioListTile(
-              value: 1, 
-              groupValue: _groupValue, 
-              onChanged: rmethod,
-              title: const Text('Dhaka'),
-              selected: true,
-              selectedTileColor: Colors.green[300],
-
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.red,width: 2),
             ),
-            RadioListTile(
-              value: 2, 
-              groupValue: _groupValue, 
-              onChanged: rmethod,
-              title: const Text('Khulna'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Slider(
+                  value: _sValue,
+                  min: 0,
+                  max: 10,
+                  onChanged: smethod,
+                  ),
+                Text('Range : $_sValue',
+                style:const TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold
+                  ) ,
+                ),
+              ],
             ),
-            RadioListTile(
-              value: 3, 
-              groupValue: _groupValue, 
-              onChanged: rmethod,
-              title: const Text('Barishal'),
-            ),
-            RadioListTile(
-              value: 4, 
-              groupValue: _groupValue, 
-              onChanged: rmethod,
-              title: const Text('Rajshahi'),
-              selected: _groupValue==4?true:false,
-            ),
-            RadioListTile(
-              value: 5, 
-              groupValue: _groupValue, 
-              onChanged: rmethod,
-              title: const Text('Rangpur'),
-              secondary: const Icon(Icons.menu_book),
-              selected: _groupValue==5?true:false,
-            ),
-            RadioListTile(
-              value: 6, 
-              groupValue: _groupValue, 
-              onChanged: rmethod,
-              title: const Text('Comilla',style: TextStyle(fontSize: 20),),
-              activeColor: Colors.red,
-              tileColor: Colors.grey[300],
-              secondary: const Icon(Icons.favorite),
-              controlAffinity: ListTileControlAffinity.trailing,
-              selected: _groupValue==6?true:false,
-            ),
-          ],
+          ),
         ),
-      ),
+        ),
     );
   }
 }
